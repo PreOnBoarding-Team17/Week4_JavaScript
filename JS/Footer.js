9dec157fbba22eadfe418016463e01e1852ded41
developjik <developjik@gmail.com>
2022-02-22 03:14:05 +0900

    feat #9: footer 영역 ie chrome 반응형 코드 추가

--- JS/Footer.js
+++ JS/Footer.js
@@ -2,3 +2,9 @@ document.querySelector('.test').addEventListener('click', function(e) {
     e.preventDefault();
     window.open('https://www.youtube.com', 'popup', 'width=800, height=600');
 });
+
+var isIE = /*@cc_on!@*/false || !!document.documentMode;
+var m = matchMedia("screen and (min-width: 1024px)");
+if (isIE && m.matches) {
+    document.querySelector('.footer__tems').style.bottom = '40px';
+} 
