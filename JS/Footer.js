a249de921b761192c8559aced9d40f8876daf317
developjik <developjik@gmail.com>
2022-02-22 17:34:14 +0900

    feat #9: ie8에 맞춰서 반응형 처리

--- JS/Footer.js
+++ JS/Footer.js
@@ -1,10 +1,35 @@
-document.querySelector('.test').addEventListener('click', function(e) {
-    e.preventDefault();
-    window.open('https://www.youtube.com', 'popup', 'width=800, height=600');
-});
+var lowIE = !window.addEventListener
+var screenWidth = document.documentElement.clientWidth;
 
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-var m = matchMedia("screen and (min-width: 1024px)");
-if (isIE && m.matches) {
+
+if(lowIE) {
+    document.querySelector('.footer__contact').style.paddingBottom = '20px';
+    document.querySelector('.footer__contact-last').style.marginTop = '20px';
+    document.querySelector('.footer__contact-pc').style.display = 'none';
+}
+
+if (lowIE && screenWidth >= 1024) {
     document.querySelector('.footer__tems').style.bottom = '40px';
+    document.querySelector('.footer__contact-last').style.display = 'none';
+    document.querySelector('.footer__contact-pc').style.display = 'inline-block';
+    document.querySelector('.footer__contact-pc').style.marginLeft = '16px';
 } 
+
+if (lowIE) {
+    window.attachEvent("onresize", function(e) {
+        var screenWidth = document.documentElement.clientWidth;
+
+        if(screenWidth < 1024) {
+            document.querySelector('.footer__contact').style.paddingBottom = '20px';
+            document.querySelector('.footer__contact-last').style.marginTop = '20px';
+            document.querySelector('.footer__contact-last').style.display = 'block';
+            document.querySelector('.footer__contact-pc').style.display = 'none';
+        }
+        else { 
+            document.querySelector('.footer__tems').style.bottom = '40px';
+            document.querySelector('.footer__contact-last').style.display = 'none';
+            document.querySelector('.footer__contact-pc').style.display = 'inline-block';
+            document.querySelector('.footer__contact-pc').style.marginLeft = '16px';
+        } 
+    })
+}
--- JS/GNB.js
+++ JS/GNB.js
@@ -1,17 +1,19 @@
-function scrollAction(y) {
-    var header = document.querySelector('header')
+function scrollAction() {
+
+    var header = document.querySelector('.header')
     var nav = document.querySelector('nav')
 
-    var screenWidth = window.innerWidth !== undefined ? window.innerWidth : document.documentElement.clientWidth;
+    var scrollY = window.pageYOffset
+    var screenWidth = window.innerWidth
 
-    if(y === 0) {
+    if(scrollY === 0) {
         header.classList.remove('is-scroll');
 
         if(nav.style.display==='none') {
             nav.style.display='block'
         }
 
-    }else if(header.classList.length ===0 ){
+    }else if(header.classList.length === 1 ){
         header.classList.add('is-scroll');
 
         if(nav.style.display!=='none' && screenWidth <= 1024) {
@@ -20,22 +22,37 @@ function scrollAction(y) {
     }  
 }
 
-window.addEventListener('scroll', function(e) {
-    var supportPageOffset = window.pageYOffset !== undefined;
+function scrollActionInIE () {
+    var header = document.querySelector('.header')
+    var nav = document.querySelector('.nav-wrapper')
+
     var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
-    var y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+    var scrollY = isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop
+    var screenWidth = document.documentElement.clientWidth;
 
-    scrollAction(y)
-});
+    if(scrollY === 0) {
+        header.style.boxShadow= '0px 0px 1px #000';
+        header.style.borderBottom= 'none';
+        
+        if(nav.style.display==='none') {
+            nav.style.display='block'
+        }
+        
+    }else{
+        header.style.borderBottom= '1px solid #ffc948';
+        header.style.boxShadow= '0px 0px 1px #ffc948';
 
-window.addEventListener('beforeunload', function(event) {
-    this.window.opener.location('https://www.youtube.com/watch?v=N0dc-idpwoE', 'popup', 'width=800, height=600');
-    event.preventDefault();
-    // Chrome에서는 returnValue 설정이 필요함
-    event.returnValue = '';
-   
-  });
-  window.addEventListener('unload', function(event) {
-    console.log('I am the 3rd one.');
-    this.alert('123')
-  });
+        if(screenWidth <= 1024) {
+            nav.style.display='none'
+        }
+    }  
+
+}
+
+if (window.addEventListener) {
+    window.addEventListener('scroll', scrollAction);
+}
+else {
+    window.attachEvent("onscroll", scrollActionInIE);
+}
+
