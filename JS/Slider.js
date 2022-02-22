006d282dc24554cb8e47effca71c4359bae53e98
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-02-22 18:18:59 +0900

    feat #3: Chip 클릭 이벤트 생성

---
+++ JS/Slider.js
@@ -0,0 +1,179 @@
+var prevButton = document.querySelector(".slider__button-left");
+var nextButton = document.querySelector(".slider__button-right");
+var mainImagePC = document.querySelector("#main-image_pc");
+var mainImageMobile = document.querySelector("#main-image_mobile");
+var subImageFirst = document.querySelector("#sub-image_first");
+var subImageSecond = document.querySelector("#sub-image_second");
+var subTitleFirst = document.querySelector("#title_first");
+var subTitleSecond = document.querySelector("#title_second");
+var subPriceFirst = document.querySelector("#price_first");
+var subPriceSecond = document.querySelector("#price_second");
+var chipListFirst = document.querySelector("#chipList_first");
+var chipListSecond = document.querySelector("#chipList_second");
+
+var i = 1;
+var title = [
+  "취미 클래스(4주 과정)",
+  "오아시스 리스(2/21~2/27)",
+  "클래식 핸드타이드(2/28~3/6)",
+];
+var price = ["299,900원", "79,000원", "79,000원"];
+var chip = [
+  [
+    { color: "cyan", place: "잠실점" },
+    { color: "orange", place: "월계점" },
+    { color: "blue", place: "구로점" },
+    { color: "yellow", place: "송파점" },
+    { color: "lightblue", place: "부산동래점" },
+  ],
+  [
+    { color: "purple", place: "광화문점" },
+    { color: "cyan", place: "잠실점" },
+    { color: "orange", place: "월계점" },
+    { color: "blue", place: "구로점" },
+    { color: "yellow", place: "송파점" },
+    { color: "lightblue", place: "부산동래점" },
+  ],
+  [
+    { color: "blue", place: "구로점" },
+    { color: "cyan", place: "잠실점" },
+    { color: "orange", place: "월계점" },
+    { color: "yellow", place: "송파점" },
+    { color: "lightblue", place: "부산동래점" },
+  ],
+];
+
+function handleChip(i) {
+  while (chipListFirst.hasChildNodes()) {
+    chipListFirst.removeChild(chipListFirst.firstChild);
+  }
+  while (chipListSecond.hasChildNodes()) {
+    chipListSecond.removeChild(chipListSecond.firstChild);
+  }
+  if (i === 3) {
+    chip[0].map((element) => {
+      var newChip = document.createElement("a");
+      newChip.innerHTML = `${element.place}`;
+      newChip.setAttribute(
+        "class",
+        `slider__sub--summary-chip ${element.color}`
+      );
+      chipListFirst.appendChild(newChip);
+    });
+    chip[1].map((element) => {
+      var newChip = document.createElement("a");
+      newChip.innerHTML = `${element.place}`;
+      newChip.setAttribute(
+        "class",
+        `slider__sub--summary-chip ${element.color}`
+      );
+      chipListSecond.appendChild(newChip);
+    });
+  } else if (i === 2) {
+    chip[2].map((element) => {
+      var newChip = document.createElement("a");
+      newChip.innerHTML = `${element.place}`;
+      newChip.setAttribute(
+        "class",
+        `slider__sub--summary-chip ${element.color}`
+      );
+      chipListFirst.appendChild(newChip);
+    });
+    chip[0].map((element) => {
+      var newChip = document.createElement("a");
+      newChip.innerHTML = `${element.place}`;
+      newChip.setAttribute(
+        "class",
+        `slider__sub--summary-chip ${element.color}`
+      );
+      chipListSecond.appendChild(newChip);
+    });
+  } else {
+    chip[1].map((element) => {
+      var newChip = document.createElement("a");
+      newChip.innerHTML = `${element.place}`;
+      newChip.setAttribute(
+        "class",
+        `slider__sub--summary-chip ${element.color}`
+      );
+      chipListFirst.appendChild(newChip);
+    });
+    chip[2].map((element) => {
+      var newChip = document.createElement("a");
+      newChip.innerHTML = `${element.place}`;
+      newChip.setAttribute(
+        "class",
+        `slider__sub--summary-chip ${element.color}`
+      );
+      chipListSecond.appendChild(newChip);
+    });
+  }
+}
+
+function handleChange(i) {
+  mainImagePC.setAttribute(
+    "style",
+    `background-image: url('./Asset/Slider/Slider_${i}.jpeg')`
+  );
+  mainImageMobile.setAttribute(
+    "style",
+    `background-image: url('./Asset/Slider/Slider_${i}.jpeg')`
+  );
+  handleChip(i);
+  if (i === 3) {
+    subImageFirst.setAttribute(
+      "style",
+      `background-image: url('./Asset/Slider/Slider_1.jpeg')`
+    );
+    subImageSecond.setAttribute(
+      "style",
+      `background-image: url('./Asset/Slider/Slider_2.jpeg')`
+    );
+    subTitleFirst.innerText = title[0];
+    subTitleSecond.innerText = title[1];
+    subPriceFirst.innerText = price[0];
+    subPriceSecond.innerText = price[1];
+  } else if (i === 2) {
+    subImageFirst.setAttribute(
+      "style",
+      `background-image: url('./Asset/Slider/Slider_3.jpeg')`
+    );
+    subImageSecond.setAttribute(
+      "style",
+      `background-image: url('./Asset/Slider/Slider_1.jpeg')`
+    );
+    subTitleFirst.innerText = title[2];
+    subTitleSecond.innerText = title[0];
+    subPriceFirst.innerText = price[2];
+    subPriceSecond.innerText = price[0];
+  } else {
+    subImageFirst.setAttribute(
+      "style",
+      `background-image: url('./Asset/Slider/Slider_2.jpeg')`
+    );
+    subImageSecond.setAttribute(
+      "style",
+      `background-image: url('./Asset/Slider/Slider_3.jpeg')`
+    );
+    subTitleFirst.innerText = title[1];
+    subTitleSecond.innerText = title[2];
+    subPriceFirst.innerText = price[1];
+    subPriceSecond.innerText = price[2];
+  }
+}
+
+nextButton.addEventListener("click", function (index) {
+  if (index) {
+    i++;
+    if (i === 4) i = 1;
+  }
+  handleChange(i);
+});
+
+prevButton.addEventListener("click", function (index) {
+  if (index) {
+    i--;
+    if (i === 0) i = 3;
+  }
+  handleChange(i);
+});
