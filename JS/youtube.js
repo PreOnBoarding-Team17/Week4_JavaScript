c68646bab74fa2b0c2d487fb576170ac470d4744
som-syom <dhyeon@student.42seoul.kr>
2022-02-22 16:05:10 +0900

    fix #4: console 에러 나는 부분 수정

--- JS/youtube.js
+++ JS/youtube.js
@@ -7,10 +7,12 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 function onYouTubeIframeAPIReady() {
   new YT.Player("youtube__player", {
     videoId: "N0dc-idpwoE",
+    host: `${window.location.protocol}//www.youtube.com`,
     playerVars: {
       autoplay: true,
       loop: true,
       playlist: "N0dc-idpwoE",
+      origin: "http://localhost:5500",
     },
     events: {
       onReady: function (event) {
